5b50a44abffdbf139a40599a4d81ba2743a6fe64
Ajay Sathya <ajay.sathya@kuleuven.be>
2020-02-25 12:10:37 +0100

    minor

--- tasho/robot_loader.py
+++ tasho/robot_loader.py
@@ -8,3 +8,17 @@ def load_fk(robot_name):
 	robot_fk = cs.Function.load(file)
 
 	return load_fk
+
+def load_inverse_dynamics(robot_name):
+
+	file = '/robots/' + robot_name + '/' + robot_name + '_id.casadi'
+	robot_id = cs.Function.load(file)
+
+	return robot_id
+
+def load_forward_dynamics(robot_name):
+
+	file = '/robots/' + robot_name + '/' + robot_name + '_fd.casadi'
+	robot_fd = cs.Function.load(file)
+
+	return robot_fd
