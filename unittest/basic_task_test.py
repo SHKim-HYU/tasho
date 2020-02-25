3b11b98cd8d022569d79a9a1d5eb2de18e35c309
Alejandro ASTUDILLO VIGOYA <alejandro.astudillovigoya@kuleuven.be>
2020-02-25 15:47:46 +0100

    added parameter change test

--- unittest/basic_task_test.py
+++ unittest/basic_task_test.py
@@ -6,13 +6,7 @@ import numpy as np
 
 class TestTask(unittest.TestCase):
 
-    def test_task(self):
-        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
-
-    # def test_sum_tuple(self):
-    #     self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
-
-    def test_task_tuple(self):
+    def test_task_hw(self):
         tc = tp.task_context(5)
 
         x = tc.create_expression('x', 'state', (1, 1))
@@ -30,7 +24,8 @@ class TestTask(unittest.TestCase):
         solver_options = {"ipopt": {"print_level": 0}, "print_time": False, "expand" : True}
 
         tc.ocp.solver('ipopt',solver_options)
-        tc.ocp.method(MultipleShooting(N = 5, M = 2, intg='rk'))
+        disc_settings = {'discretization method': 'multiple shooting', 'horizon size': 5, 'order':2, 'integration':'rk'}
+        tc.set_discretization_settings(disc_settings)
 
         ocp = tc.ocp
         sol = ocp.solve()
@@ -40,6 +35,46 @@ class TestTask(unittest.TestCase):
 
         self.assertAlmostEqual( x_val[-1], 2.236067977499, 10, "Final position test failed")
         self.assertEqual( t[-1], 5, "Final time test failed")
+
+    def test_task_hw_tuple(self):
+        tc = tp.task_context(5)
+
+        x = tc.create_expression('x', 'state', (1, 1))
+
+        u = tc.create_expression('u', 'control', (1, 1))
+
+        p = tc.create_expression('p', 'parameter', (1, 1))
+
+        tc.set_dynamics(x, u)
+
+        task_spec = {'initial_constraints':[{'expression':x, 'reference':0.05}]}
+        task_spec['path_constraints'] = [{'expression':u, 'reference':0, 'hard':False, 'gain':1}]
+        task_spec['final_constraints'] = [{'expression':x**2, 'reference':p, 'hard':True}]
+
+        tc.add_task_constraint(task_spec)
+
+        solver_options = {"ipopt": {"print_level": 0}, "print_time": False, "expand" : True}
+
+        tc.ocp.solver('ipopt',solver_options)
+        disc_settings = {'discretization method': 'multiple shooting', 'horizon size': 5, 'order':2, 'integration':'rk'}
+        tc.set_discretization_settings(disc_settings)
+
+        ocp = tc.ocp
+        ocp.set_value(p, 5)
+        sol = ocp.solve()
+        t, x_val= sol.sample(x, grid='control')
+
+        self.assertAlmostEqual( x_val[-1], 2.236067977499, 10, "Final position test failed")
+        self.assertEqual( t[-1], 5, "Final time test failed")
+
+        # Test parameter change
+        ocp.set_value(p, 0.9)
+        sol = ocp.solve()
+        t, x_val= sol.sample(x, grid='control')
+        self.assertAlmostEqual( x_val[-1], 0.9486832980505, 10, "Final position test failed")
+        # self.assertEqual( t[-1], 5, "Final time test failed")
+
+        tc.generate_function(name = 'opti_o', save=True, codegen=False)
 
     def test_robotloader(self):
         # Kinova Gen3
