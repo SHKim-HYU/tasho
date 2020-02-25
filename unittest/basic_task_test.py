e2a286d5d215fcbd677926bc3303e7944e4c594c
Alejandro ASTUDILLO VIGOYA <alejandro.astudillovigoya@kuleuven.be>
2020-02-25 15:57:03 +0100

    minor

--- unittest/basic_task_test.py
+++ unittest/basic_task_test.py
@@ -14,36 +14,6 @@ class TestTask(unittest.TestCase):
 
         u = tc.create_expression('u', 'control', (1, 1))
 
-        tc.set_dynamics(x, u)
-
-        task_spec = {'initial_constraints':[{'expression':x, 'reference':0.1}]}
-        task_spec['path_constraints'] = [{'expression':u, 'reference':0, 'hard':False, 'gain':1}]
-        task_spec['final_constraints'] = [{'expression':x**2, 'reference':5, 'hard':True}]
-
-        tc.add_task_constraint(task_spec)
-
-        solver_options = {"ipopt": {"print_level": 0}, "print_time": False, "expand" : True}
-
-        tc.ocp.solver('ipopt',solver_options)
-        disc_settings = {'discretization method': 'multiple shooting', 'horizon size': 5, 'order':2, 'integration':'rk'}
-        tc.set_discretization_settings(disc_settings)
-
-        ocp = tc.ocp
-        sol = ocp.solve()
-        t, x_val= sol.sample(x, grid='control')
-
-        # tc.generate_function(name = 'opti_o', save=True, codegen=False)
-
-        self.assertAlmostEqual( x_val[-1], 2.236067977499, 10, "Final position test failed")
-        self.assertEqual( t[-1], 5, "Final time test failed")
-
-    def test_task_hw_tuple(self):
-        tc = tp.task_context(5)
-
-        x = tc.create_expression('x', 'state', (1, 1))
-
-        u = tc.create_expression('u', 'control', (1, 1))
-
         p = tc.create_expression('p', 'parameter', (1, 1))
 
         tc.set_dynamics(x, u)
