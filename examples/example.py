3278286aea6e5c678900e015a278a2cf68080b8b
Alejandro ASTUDILLO VIGOYA <alejandro.astudillovigoya@kuleuven.be>
2020-02-26 13:55:07 +0100

    minor

--- examples/example.py
+++ examples/example.py
@@ -1,7 +1,7 @@
-from tasho import problem
-
-if __name__ == '__main__':
-    ocp = problem.Problem("P2P")
-    ocp.about()
-
-    problem.world()
+# from tasho import problem
+#
+# if __name__ == '__main__':
+#     ocp = problem.Problem("P2P")
+#     ocp.about()
+#
+#     problem.world()
