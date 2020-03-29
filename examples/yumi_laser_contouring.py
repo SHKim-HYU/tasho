b04defe8dd7b97fd515b19e84b5907dd92caf921
Ajay Sathya <ajay.sathya@kuleuven.be>
2020-03-29 19:14:02 +0200

    laser tracing example added with TaSHo

--- examples/yumi_laser_contouring.py
+++ examples/yumi_laser_contouring.py
@@ -1,5 +1,6 @@
 import sys
 from tasho import task_prototype_rockit as tp
+from tasho import input_resolution
 from tasho import robot as rob
 import casadi as cs
 from casadi import pi, cos, sin
@@ -23,13 +24,13 @@ if __name__ == '__main__':
 
 	print("Task specification and visualization of MPC control of laser contouring task")
 
-	horizon_size = 10
-	t_mpc = 0.1 #the MPC sampling time
+	horizon_size = 20
+	t_mpc = 0.5 #the MPC sampling time
 	max_joint_vel = 30*3.14159/180
 	max_joint_acc = 30*3.14159/180
 
 	#TODO: remove below line after pinocchio starts to provide the robot joint limits
-	rob_settings = {'n_dof' : 18, 'no_links' : 20, 'q_min' : [-2.9409, -2.5045, -2.9409, -2.1555, -5.0615, -1.5359, -3.9968, 0, 0, -2.9409, -2.5045, -2.9409, -2.1555, -5.0615, -1.5359, -3.9968, 0, 0], 'q_max' : [2.9409, 0.7592, 2.9409, 1.3963, 5.0615, 2.4086, 3.9968, 0.025, 0.025, 2.9409, 0.7592, 2.9409, 1.3963, 5.0615, 2.4086, 3.9968, 0.025, 0.025] }
+	rob_settings = {'n_dof' : 18, 'no_links' : 20, 'q_min' : np.array([-2.9409, -2.5045, -2.9409, -2.1555, -5.0615, -1.5359, -3.9968, 0, 0, -2.9409, -2.5045, -2.9409, -2.1555, -5.0615, -1.5359, -3.9968, 0, 0]).T, 'q_max' : np.array([2.9409, 0.7592, 2.9409, 1.3963, 5.0615, 2.4086, 3.9968, 0.025, 0.025, 2.9409, 0.7592, 2.9409, 1.3963, 5.0615, 2.4086, 3.9968, 0.025, 0.025]).T }
 	robot = rob.Robot('yumi')
 	robot.set_from_json('yumi.json')
 
@@ -38,6 +39,7 @@ if __name__ == '__main__':
 	robot.set_joint_velocity_limits(lb = -max_joint_vel, ub = max_joint_vel)
 	robot.set_joint_acceleration_limits(lb = -max_joint_acc, ub = max_joint_acc)
 
+	print(robot.joint_ub)
 	print(robot.joint_ub)
 
 	tc = tp.task_context(horizon_size*t_mpc)
@@ -54,10 +56,12 @@ if __name__ == '__main__':
 	tc.set_dynamics(s, s_dot)
 
 
-	pos_limits = {'lub':True, 'hard': True, 'expression':q, 'upper_limits':robot.joint_ub, 'lower_limits':robot.joint_lb}
+	#TODO: change to using limtis from robot object!
+	pos_limits = {'lub':True, 'hard': True, 'expression':q, 'upper_limits':rob_settings['q_max'], 'lower_limits':rob_settings['q_min']}
 	vel_limits = {'lub':True, 'hard': True, 'expression':q_dot, 'upper_limits':robot.joint_vel_ub, 'lower_limits':robot.joint_vel_lb}
 	acc_limits = {'lub':True, 'hard': True, 'expression':q_ddot, 'upper_limits':robot.joint_acc_ub, 'lower_limits':robot.joint_acc_lb}
 	joint_constraints = {'path_constraints':[pos_limits, vel_limits, acc_limits]}
+	# joint_constraints = {'path_constraints':[pos_limits]}
 	tc.add_task_constraint(joint_constraints)
 
 	#parameters of the ocp
@@ -70,6 +74,7 @@ if __name__ == '__main__':
 	s_init_con = {'expression':s, 'reference':0}
 	s_dot_init_con = {'expression':s_dot, 'reference':0}
 	init_constraints = {'initial_constraints':[joint_init_con, joint_vel_init_con, s_init_con, s_dot_init_con]}
+	# init_constraints = {'initial_constraints':[joint_init_con]}
 	tc.add_task_constraint(init_constraints)
 
 	#computing the forward kinematics of the robot tree
@@ -94,25 +99,46 @@ if __name__ == '__main__':
 
 	contour_error = {'equality':True, 'hard': False, 'expression':Ps, 'reference':p_des, 'gain':10}
 	vel_regularization = {'hard': False, 'expression':q_dot, 'reference':0, 'gain':1}
-	s_dot_regularization = {'hard': False, 'expression':s_dot, 'reference':0, 'gain':1}
-	task_objective = {'path_constraints':[vel_regularization, s_dot_regularization]}
-	#task_objective = {'path_constraints':[contour_error, vel_regularization, s_dot_regularization]}
+	s_dot_regularization = {'hard': False, 'expression':s_dot, 'reference':0, 'gain':0.01}
+	s_con = {'hard':True, 'inequality':True, 'expression':-s, 'upper_limits':0}
+	# task_objective = {'path_constraints':[vel_regularization, s_dot_regularization, s_con]}
+	task_objective = {'path_constraints':[contour_error, vel_regularization, s_dot_regularization, s_con]}
+	
+
+	#Add path constraints on the depth and the angle of the laser interception
+	dot_prod_ee_workpiece = -cs.mtimes(Ns.T, Nl)
+	#constraint on the angle between laser pointer and workpiece normal
+	angle_limit = 10*3.14159/180
+	angle_constraint = {'hard':True, 'inequality':True, 'expression':-dot_prod_ee_workpiece, 'upper_limits':angle_limit}
+	#constraint on the distance between laser pointer and the workpiece
+	distance_constraint = {'hard':True, 'lub':True, 'expression':a, 'upper_limits':0.02, 'lower_limits':0.0}
+	task_objective['path_constraints'].append(angle_constraint)
+	task_objective['path_constraints'].append(distance_constraint)
+
 	tc.add_task_constraint(task_objective)
 
 	#adding the final constraints
 	final_vel = {'hard':True, 'expression':q_dot, 'reference':0}
-	final_s = {'hard':True, 'expression':s, 'reference':2*pi}
+	final_s = {'hard':True, 'expression':s, 'reference':6.28}
 	final_constraints = {'final_constraints':[final_vel, final_s]}
 	tc.add_task_constraint(final_constraints)
-	q0_contour = [-1.35488912e+00, -8.72846052e-01, 2.18411843e+00,  6.78786296e-01,
+	q0_contour = np.array([-1.35488912e+00, -8.72846052e-01, 2.18411843e+00,  6.78786296e-01,
   2.08696971e+00, -9.76390128e-01, -1.71721329e+00,  1.65969745e-03,
   1.65969745e-03,  1.47829337e+00, -5.24943547e-01, -1.95134781e+00,
   5.30517837e-01, -2.69960026e+00, -8.14070355e-01,  1.17172289e+00,
-  2.06459136e-03,  2.06462524e-03]
+  2.06459136e-03,  2.06462524e-03]).T
 
-	tc.set_ocp_solver('ipopt')
+	tc.set_ocp_solver('ipopt', {'ipopt':{"max_iter": 1000, 'hessian_approximation':'limited-memory', 'limited_memory_max_history' : 5, 'tol':1e-3}})
+	# tc.set_ocp_solver('ipopt', {'ipopt':{"max_iter": 1000, 'tol':1e-3}})
 	tc.ocp.set_value(q0, q0_contour)
 	tc.ocp.set_value(q_dot0, [0]*18)
 	disc_settings = {'discretization method': 'multiple shooting', 'horizon size': 10, 'order':1, 'integration':'rk'}
 	tc.set_discretization_settings(disc_settings)
-	sol = tc.solve_ocp()
+	try:
+		sol = tc.solve_ocp()
+		# print(sol.sample(q, grid="control"))
+	except:
+		tc.ocp.show_infeasibilities(1e-3)
+		sol = tc.ocp.debug
+		sol = tc.ocp.opti.debug
+		# print(sol.value(tc.ocp._method.eval_at_control(tc.ocp, q, 0)))
