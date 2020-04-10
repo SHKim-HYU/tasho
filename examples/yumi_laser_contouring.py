08b1bb86a4f448946e4cb5435a28fe40e3b27cd1
Ajay Sathya <ajay.sathya@kuleuven.be>
2020-04-10 22:48:34 +0200

    added bullet visualization to laser contouring OCP

--- examples/yumi_laser_contouring.py
+++ examples/yumi_laser_contouring.py
@@ -8,6 +8,8 @@ from rockit import MultipleShooting, Ocp
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Yumi laser tracing OCP with visualization in bullet
+
 def inv_T_matrix(T):
 
 	T_inv = cs.horzcat(cs.horzcat(T[0:3, 0:3].T, cs.mtimes(-T[0:3, 0:3].T, T[0:3,3])).T, [0, 0, 0, 1]).T
@@ -24,8 +26,10 @@ if __name__ == '__main__':
 
 	print("Task specification and visualization of MPC control of laser contouring task")
 
+	visualizationBullet = True #toggle visualization with PyBullet option
+
 	horizon_size = 20
-	t_mpc = 0.5 #the MPC sampling time
+	t_mpc = 0.1 #the MPC sampling time
 	max_joint_vel = 30*3.14159/180
 	max_joint_acc = 30*3.14159/180
 
@@ -33,7 +37,7 @@ if __name__ == '__main__':
 	rob_settings = {'n_dof' : 18, 'no_links' : 20, 'q_min' : np.array([-2.9409, -2.5045, -2.9409, -2.1555, -5.0615, -1.5359, -3.9968, 0, 0, -2.9409, -2.5045, -2.9409, -2.1555, -5.0615, -1.5359, -3.9968, 0, 0]).T, 'q_max' : np.array([2.9409, 0.7592, 2.9409, 1.3963, 5.0615, 2.4086, 3.9968, 0.025, 0.025, 2.9409, 0.7592, 2.9409, 1.3963, 5.0615, 2.4086, 3.9968, 0.025, 0.025]).T }
 	robot = rob.Robot('yumi')
 	robot.set_from_json('yumi.json')
-
+	print(robot.joint_name)
 	## Customise robot limits
 	# robot.set_joint_limits(lb = rob_settings['q_min'], ub = rob_settings['q_max'])
 	robot.set_joint_velocity_limits(lb = -max_joint_vel, ub = max_joint_vel)
@@ -132,13 +136,49 @@ if __name__ == '__main__':
 	# tc.set_ocp_solver('ipopt', {'ipopt':{"max_iter": 1000, 'tol':1e-3}})
 	tc.ocp.set_value(q0, q0_contour)
 	tc.ocp.set_value(q_dot0, [0]*18)
-	disc_settings = {'discretization method': 'multiple shooting', 'horizon size': 10, 'order':1, 'integration':'rk'}
+	disc_settings = {'discretization method': 'multiple shooting', 'horizon size': horizon_size, 'order':1, 'integration':'rk'}
 	tc.set_discretization_settings(disc_settings)
 	try:
 		sol = tc.solve_ocp()
-		# print(sol.sample(q, grid="control"))
+		#print(sol.sample(q, grid="control"))
 	except:
 		tc.ocp.show_infeasibilities(1e-3)
 		sol = tc.ocp.debug
 		sol = tc.ocp.opti.debug
 		# print(sol.value(tc.ocp._method.eval_at_control(tc.ocp, q, 0)))
+
+
+	if visualizationBullet:
+
+		from tasho import world_simulator
+
+		obj = world_simulator.world_simulator()
+
+		position = [0.0, 0.0, 0.0]
+		orientation = [0.0, 0.0, 0.0, 1.0]
+
+		yumiID = obj.add_robot(position, orientation, 'yumi')
+		no_samples = int(t_mpc/obj.physics_ts)
+
+		if no_samples != t_mpc/obj.physics_ts:
+			print("[ERROR] MPC sampling time not integer multiple of physics sampling time")
+
+		#correspondence between joint numbers in bullet and OCP determined after reading joint info of YUMI
+		#from the world simulator
+		joint_indices = [11, 12, 13, 14, 15, 16, 17, 18, 19, 1, 2, 3, 4, 5, 6, 7, 8, 9] 
+
+		#begin the visualization of applying OCP solution in open loop
+		ts, q_dot_sol = sol.sample(q_dot, grid="control")
+		obj.resetJointState(yumiID, joint_indices, q0_contour)
+		obj.setController(yumiID, "velocity", joint_indices, targetVelocities = q_dot_sol[0])
+		obj.run_simulation(480)
+
+
+		for i in range(horizon_size):
+			q_vel_current = 0.5*(q_dot_sol[i] + q_dot_sol[i+1])
+			obj.setController(yumiID, "velocity", joint_indices, targetVelocities = q_vel_current)
+			obj.run_simulation(no_samples)
+
+		obj.setController(yumiID, "velocity", joint_indices, targetVelocities = q_dot_sol[0])
+		obj.run_simulation(1000)
+		obj.end_simulation()
