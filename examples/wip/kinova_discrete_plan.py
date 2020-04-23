4d22eae6bc34020b5ed473b6b1913cafd040867e
Alejandro ASTUDILLO VIGOYA <alejandro.astudillovigoya@kuleuven.be>
2020-04-23 21:46:57 +0200

    Added simulate_plan to discrete_plan.py (#14)

--- examples/wip/kinova_discrete_plan.py
+++ examples/wip/kinova_discrete_plan.py
@@ -11,108 +11,105 @@ from tasho import discrete_plan as dp
 
 if __name__ == '__main__':
 
-	print("Random bin picking with Kinova Gen3")
+    print("Random bin picking with Kinova Gen3")
 
-	visualizationBullet = True
-	horizon_size = 10
-	t_mpc = 0.5
-	max_joint_acc = 30*3.14159/180
+    visualizationBullet = True
+    horizon_size = 10
+    t_mpc = 0.5
+    max_joint_acc = 30*3.14159/180
 
-	plan = dp.DiscretePlan()
+    q0_val = [0, -0.523598, 0, 2.51799, 0, -0.523598, -1.5708]
 
-	robot = rob.Robot('kinova')
-	robot.set_from_json('kinova.json')
-	robot.set_joint_acceleration_limits(lb = -max_joint_acc, ub = max_joint_acc)
-	print(robot.joint_name)
-	print(robot.joint_ub)
-	print(robot.joint_lb)
+    plan = dp.DiscretePlan()
 
-	# --------------------------------------------------------------------------
-	# Approximation to object
-	# --------------------------------------------------------------------------
-	tc = tp.task_context(horizon_size*t_mpc)
-
-	q, q_dot, q_ddot, q0, q_dot0 = input_resolution.acceleration_resolved(tc, robot, {})
-
-	#computing the expression for the final frame
-	print(robot.fk)
-	fk_vals = robot.fk(q)[7]
-	print(fk_vals[0:2,3])
-
-	# T_goal = np.array([[0.0, 0., -1., 0.5], [0., 1., 0., 0.], [1.0, 0., 0.0, 0.5], [0.0, 0.0, 0.0, 1.0]])
-	# T_goal = np.array([[0., 0., -1., 0.5], [-1., 0., 0., 0.], [0., 1., 0.0, 0.5], [0.0, 0.0, 0.0, 1.0]])
-	# T_goal = np.array([[0., 1., 0., 0.5], [1., 0., 0., 0.], [0., 0., -1.0, 0.5], [0.0, 0.0, 0.0, 1.0]])
-	T_goal = np.array([[0, 1, 0, 0.5], [1, 0, 0, 0], [0, 0, -1, 0.25], [0, 0, 0, 1]])
-	# T_goal = np.array([[0, 1, 0, 0], [1, 0, 0, -0.5], [0, 0, -1, 0.5], [0, 0, 0, 1]])
-	final_pos = {'hard':True, 'type':'Frame', 'expression':fk_vals, 'reference':T_goal}
-	final_vel = {'hard':True, 'expression':q_dot, 'reference':0}
-	final_constraints = {'final_constraints':[final_pos, final_vel]}
-	tc.add_task_constraint(final_constraints)
-
-	#adding penality terms on joint velocity and position
-	vel_regularization = {'hard': False, 'expression':q_dot, 'reference':0, 'gain':1}
-	acc_regularization = {'hard': False, 'expression':q_ddot, 'reference':0, 'gain':1}
-
-	task_objective = {'path_constraints':[vel_regularization, acc_regularization]}
-	tc.add_task_constraint(task_objective)
-
-	tc.set_ocp_solver('ipopt', {'ipopt':{"max_iter": 1000, 'hessian_approximation':'limited-memory', 'limited_memory_max_history' : 5, 'tol':1e-3}})
-	# q0_val = [0]*7
-	# q0_val = [0, -3.1416/6, 0, 3*3.1416/6, 0, 3.1416/6, 0]
-	# q0_val = [0, 3.1416/6, 0, 4*3.1416/6, 0, -2*3.1416/6, -1.5708]
-	# q0_val = [0, 2.8942, 1.0627, 1.4351,-0.7789, 1.4801, -3.0595]
-	# q0_val = [0, -0.523598, 3.141592654, -2.61799, 3.141592654, -0.523598, -1.5708]
-	q0_val = [0, -0.523598, 0, 2.51799, 0, -0.523598, -1.5708]
-	# q0_val = [0, 0.3491, 0, 2.0944, 0, 0.6981, -1.5708]
-
-	disc_settings = {'discretization method': 'multiple shooting', 'horizon size': horizon_size, 'order':1, 'integration':'rk'}
-	tc.set_discretization_settings(disc_settings)
+    robot = rob.Robot('kinova')
+    robot.set_from_json('kinova.json')
+    robot.set_joint_acceleration_limits(lb = -max_joint_acc, ub = max_joint_acc)
+    print(robot.joint_name)
+    print(robot.joint_ub)
+    print(robot.joint_lb)
 
     # --------------------------------------------------------------------------
-	# Picking the object up
-	# --------------------------------------------------------------------------
+    # Approximation to object
+    # --------------------------------------------------------------------------
+    tc = tp.task_context(horizon_size*t_mpc)
 
-	horizon_size_pickup = 16
+    q, q_dot, q_ddot, q0, q_dot0 = input_resolution.acceleration_resolved(tc, robot, {})
 
-	tc_pickup = tp.task_context(horizon_size_pickup*t_mpc)
+    #computing the expression for the final frame
+    print(robot.fk)
+    fk_vals = robot.fk(q)[7]
+    print(fk_vals[0:2,3])
 
-	q_pickup, q_dot_pickup, q_ddot_pickup, q0_pickup, q_dot0_pickup = input_resolution.acceleration_resolved(tc_pickup, robot, {})
+    # T_goal = np.array([[0.0, 0., -1., 0.5], [0., 1., 0., 0.], [1.0, 0., 0.0, 0.5], [0.0, 0.0, 0.0, 1.0]])
+    # T_goal = np.array([[0., 0., -1., 0.5], [-1., 0., 0., 0.], [0., 1., 0.0, 0.5], [0.0, 0.0, 0.0, 1.0]])
+    # T_goal = np.array([[0., 1., 0., 0.5], [1., 0., 0., 0.], [0., 0., -1.0, 0.5], [0.0, 0.0, 0.0, 1.0]])
+    T_goal = np.array([[0, 1, 0, 0.5], [1, 0, 0, 0], [0, 0, -1, 0.25], [0, 0, 0, 1]])
+    # T_goal = np.array([[0, 1, 0, 0], [1, 0, 0, -0.5], [0, 0, -1, 0.5], [0, 0, 0, 1]])
+    final_pos = {'hard':True, 'type':'Frame', 'expression':fk_vals, 'reference':T_goal}
+    final_vel = {'hard':True, 'expression':q_dot, 'reference':0}
+    final_constraints = {'final_constraints':[final_pos, final_vel]}
+    tc.add_task_constraint(final_constraints)
 
-	#computing the expression for the final frame
-	print(robot.fk)
-	fk_vals = robot.fk(q_pickup)[7]
+    #adding penality terms on joint velocity and position
+    vel_regularization = {'hard': False, 'expression':q_dot, 'reference':0, 'gain':1}
+    acc_regularization = {'hard': False, 'expression':q_ddot, 'reference':0, 'gain':1}
 
-	T_goal_pickup = np.array([[1, 0, 0, 0], [0, -1, 0, -0.5], [0, 0, -1, 0.25], [0, 0, 0, 1]])
-	# T_goal_pickup = np.array([[0, 1, 0, 0], [1, 0, 0, -0.5], [0, 0, -1, 0.25], [0, 0, 0, 1]])
-	final_pos_pickup = {'hard':True, 'type':'Frame', 'expression':fk_vals, 'reference':T_goal_pickup}
-	final_vel_pickup = {'hard':True, 'expression':q_dot_pickup, 'reference':0}
-	final_constraints_pickup = {'final_constraints':[final_pos_pickup, final_vel_pickup]}
-	tc_pickup.add_task_constraint(final_constraints_pickup)
+    task_objective = {'path_constraints':[vel_regularization, acc_regularization]}
+    tc.add_task_constraint(task_objective)
 
-	#adding penality terms on joint velocity and position
-	vel_regularization = {'hard': False, 'expression':q_dot_pickup, 'reference':0, 'gain':1}
-	acc_regularization = {'hard': False, 'expression':q_ddot_pickup, 'reference':0, 'gain':1}
+    tc.set_ocp_solver('ipopt', {'ipopt':{"max_iter": 1000, 'hessian_approximation':'limited-memory', 'limited_memory_max_history' : 5, 'tol':1e-3}})
 
-	task_objective_pickup = {'path_constraints':[vel_regularization, acc_regularization]}
-	tc_pickup.add_task_constraint(task_objective_pickup)
+    disc_settings = {'discretization method': 'multiple shooting', 'horizon size': horizon_size, 'order':1, 'integration':'rk'}
+    tc.set_discretization_settings(disc_settings)
 
-	tc_pickup.set_ocp_solver('ipopt', {'ipopt':{"max_iter": 1000, 'hessian_approximation':'limited-memory', 'limited_memory_max_history' : 5, 'tol':1e-3}})
-	# q0_val_pickup = final_qsol_approx
-	# tc_pickup.ocp.set_value(q0_pickup, q0_val_pickup)
-	# tc_pickup.ocp.set_value(q_dot0_pickup, [0]*7)
-	disc_settings_pickup = {'discretization method': 'multiple shooting', 'horizon size': horizon_size_pickup, 'order':1, 'integration':'rk'}
-	tc_pickup.set_discretization_settings(disc_settings_pickup)
+    # --------------------------------------------------------------------------
+    # Picking the object up
+    # --------------------------------------------------------------------------
 
-	plan.add_task(tc, "approach")
-	plan.add_task(tc_pickup, "pickup")
+    horizon_size_pickup = 16
+
+    tc_pickup = tp.task_context(horizon_size_pickup*t_mpc)
+
+    q_pickup, q_dot_pickup, q_ddot_pickup, q0_pickup, q_dot0_pickup = input_resolution.acceleration_resolved(tc_pickup, robot, {})
+
+    #computing the expression for the final frame
+    print(robot.fk)
+    fk_vals = robot.fk(q_pickup)[7]
+
+    T_goal_pickup = np.array([[1, 0, 0, 0], [0, -1, 0, -0.5], [0, 0, -1, 0.25], [0, 0, 0, 1]])
+    # T_goal_pickup = np.array([[0, 1, 0, 0], [1, 0, 0, -0.5], [0, 0, -1, 0.25], [0, 0, 0, 1]])
+    final_pos_pickup = {'hard':True, 'type':'Frame', 'expression':fk_vals, 'reference':T_goal_pickup}
+    final_vel_pickup = {'hard':True, 'expression':q_dot_pickup, 'reference':0}
+    final_constraints_pickup = {'final_constraints':[final_pos_pickup, final_vel_pickup]}
+    tc_pickup.add_task_constraint(final_constraints_pickup)
+
+    #adding penality terms on joint velocity and position
+    vel_regularization = {'hard': False, 'expression':q_dot_pickup, 'reference':0, 'gain':1}
+    acc_regularization = {'hard': False, 'expression':q_ddot_pickup, 'reference':0, 'gain':1}
+
+    task_objective_pickup = {'path_constraints':[vel_regularization, acc_regularization]}
+    tc_pickup.add_task_constraint(task_objective_pickup)
+
+    tc_pickup.set_ocp_solver('ipopt', {'ipopt':{"max_iter": 1000, 'hessian_approximation':'limited-memory', 'limited_memory_max_history' : 5, 'tol':1e-3}})
+    disc_settings_pickup = {'discretization method': 'multiple shooting', 'horizon size': horizon_size_pickup, 'order':1, 'integration':'rk'}
+    tc_pickup.set_discretization_settings(disc_settings_pickup)
 
 
-	plan.print_tasks()
+    # --------------------------------------------------------------------------
+    # Define discrete plan
+    # --------------------------------------------------------------------------
+    plan.add_task(tc, "approach")
+    plan.add_task(tc_pickup, "pickup")
 
-	sol_list = plan.solve_task(["approach","pickup"])
+    plan.print_tasks()
 
-	# sol = plan.solve_task("approach")
+    # sol = plan.solve_task(task_name = "approach", q_init = q0_val)
+    # sol_list = plan.solve_task(task_name = ["approach","pickup"], q_init = q0_val)
+    # sol_list = plan.execute_plan(q_init = q0_val)
 
-	# sol_list = plan.execute_plan()
+    # --------------------------------------------------------------------------
+    # Simulate plan execution
+    # --------------------------------------------------------------------------
 
-	# plan.simulate_plan()
+    plan.simulate_plan(simulator = "bullet", q_init = q0_val)
--- tasho/discrete_plan.py
+++ tasho/discrete_plan.py
@@ -42,10 +42,14 @@ class DiscretePlan:
             self.task_names.append(name)
 
     def print_tasks(self):
+        """Prints the list of task names."""
         print(self.task_names)
 
     def solve_task(self, task_name = None, q_init = None, qdot_init = None):
         # TODO: Check if there's any better way to get ndof from tc
+
+        # Check that the task list is not empty
+        assert (len(self.task_list) > 0),"Task list is empty"
 
         ndof = self.task_list[0].states["q"].size(1)
 
@@ -68,6 +72,9 @@ class DiscretePlan:
             sol = tc.solve_ocp()
 
         elif type(task_name) is list:
+            # Check that the task name list is not empty
+            assert (len(task_name) > 0),"Task name list is empty"
+
             sol = []
             for tname in task_name:
                 task_index =  self.task_names.index(tname)
@@ -89,6 +96,9 @@ class DiscretePlan:
 
 
     def execute_plan(self, q_init = None, qdot_init = None):
+        # Check that the task list is not empty
+        assert (len(self.task_list) > 0),"Task list is empty"
+
         solution_list = []
 
         # TODO: Check if there's any better way to get ndof from tc
@@ -114,12 +124,66 @@ class DiscretePlan:
 
         return solution_list
 
-    def simulate_plan(self, simulator="bullet"):
-        # TODO
+    def simulate_plan(self, simulator="bullet", q_init = None, qdot_init = None):
+        # Check that the task list is not empty
+        assert (len(self.task_list) > 0),"Task list is empty"
+
+        # TODO: This data should be accessible from each tc
+        t_mpc = 0.5
+        horizon_size = 10
+        horizon_size_pickup = 16
+
+
+        # TODO: Check if there's any better way to get ndof from tc
+        ndof = self.task_list[0].states["q"].size(1)
+
+        if q_init == None:
+            q_init = [0]*ndof
+        if qdot_init == None:
+            qdot_init = [0]*ndof
 
         if simulator=="bullet":
 
-            for tc in self.task_list:
-                print("")
+            # ------------------------------------------------------------------
+            # Define world for simulation
+            # ------------------------------------------------------------------
+            from tasho import world_simulator
+            import pybullet as p
+            obj = world_simulator.world_simulator()
+
+            position = [0.0, 0.0, 0.0]
+            orientation = [0.0, 0.0, 0.0, 1.0]
+
+            robotID = obj.add_robot(position, orientation, 'kinova')
+            joint_indices = [0, 1, 2, 3, 4, 5, 6]
+
+            cylID = p.loadURDF("robots/objects/cube_small.urdf", [0.5, 0, 0.35], [0.0, 0.0, 0.0, 1.0], globalScaling = 1.0)
+            tbID = p.loadURDF("robots/objects/table.urdf", [0.5, 0, 0], p.getQuaternionFromEuler([0,0,1.5708]), globalScaling = 0.3)
+            tbID2 = p.loadURDF("robots/objects/table.urdf", [0, -0.5, 0], [0.0, 0.0, 0.0, 1.0], globalScaling = 0.3)
+
+            no_samples = int(t_mpc/obj.physics_ts)
+
+            obj.resetJointState(robotID, joint_indices, q_init)
+            obj.setController(robotID, "velocity", joint_indices, targetVelocities = [0]*ndof)
+            obj.run_simulation(1000) # Here, the robot is just waiting to start the task
+
+            # Solve the task ocp's
+            sol_list = self.execute_plan(q_init = q_init, qdot_init = qdot_init)
+
+            for sol in sol_list:
+                tc = self.task_list[sol_list.index(sol)]
+                ts, q_sol = sol.sample(tc.states["q"], grid="control")
+                ts, qdot_sol = sol.sample(tc.states["q_dot"], grid="control")
+
+                horizon_size = int(qdot_sol.size/ndof - 1)
+
+                for i in range(horizon_size):
+                	q_vel_current = 0.5*(qdot_sol[i] + qdot_sol[i+1])
+                	obj.setController(robotID, "velocity", joint_indices, targetVelocities = q_vel_current)
+                	obj.run_simulation(no_samples)
+
+            obj.setController(robotID, "velocity", joint_indices, targetVelocities = [0]*ndof)
+            obj.run_simulation(1000)
+
         else:
             print("Not yet implemented")
