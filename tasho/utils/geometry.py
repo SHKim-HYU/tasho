44d51e7dfa978e68776224aefd4f5975a1836bf8
ajay <ajay.sathya@kuleuven.be>
2021-08-08 02:33:43 +0200

    minor

--- tasho/utils/geometry.py
+++ tasho/utils/geometry.py
@@ -17,10 +17,13 @@ def cross_vec2mat(v, format="MX"):
     cross product matrix"""
 
     if format == "MX":
-        R = cs.MX.zeros(3, 3)
+        R = cs.MX(3, 3)
 
     elif format == "SX":
-        R = cs.SX.zeros(3, 3)
+        R = cs.SX(3, 3)
+
+    elif format == "DM":
+        R = cs.DM(3, 3)
 
     R[0, 1] = -v[2]
     R[0, 2] = v[1]
@@ -36,45 +39,35 @@ def cross_mat2vec(W, format="MX"):
 
     """ Takes in a skew-symmetric cross product matrix and returns a vector"""
 
-    if format == "MX":
-        v = cs.MX.zeros(3, 1)
-
-    elif format == "SX":
-        v = cs.SX.zeros(3, 1)
-
-    v[0] = W[2, 1]
-    v[1] = W[0, 2]
-    v[2] = W[1, 0]
+    v = cs.vertcat(W[2, 1],
+        W[0, 2],
+        W[1, 0])
 
     return v
 
 
-def cross_vec2vec(x, y, format="MX"):
+def cross_prod(x, y, format="MX"):
 
     """ Takes in two vectors and returns their cross product"""
 
-    if format == "MX":
-        v = cs.MX.zeros(3, 1)
-    elif format == "SX":
-        v = cs.SX.zeros(3, 1)
-
-    v[0] = x[1] * y[2] - x[2] * y[1]
-    v[1] = x[2] * y[0] - x[0] * y[2]
-    v[2] = x[0] * y[1] - x[1] * y[0]
+    v = cs.vertcat(x[1] * y[2] - x[2] * y[1],
+        x[2] * y[0] - x[0] * y[2],
+        x[0] * y[1] - x[1] * y[0])
 
     return v
 
 
-def rotmat_to_axisangle(R):
+def rotmat_to_axisangle(R, reg = 1e-2):
 
     cos_theta = (R[0, 0] + R[1, 1] + R[2, 2] - 1) / 2
     unnormalized_axis = cs.vertcat(
         (R[2, 1] - R[1, 2]), (R[0, 2] - R[2, 0]), (R[1, 0] - R[0, 1])
     )
-    sin_theta = (cs.sqrt(cs.sumsqr(unnormalized_axis + 1e-4)) - 1e-2) / 2
+    sin_theta = (cs.sqrt(cs.sumsqr(unnormalized_axis)+reg) - reg**0.5) / 2
     # Adding by a small number to avoid singularity
 
     theta = cs.atan2(sin_theta, cos_theta)
-    axis = unnormalized_axis / 2 / sin_theta
+    axis = unnormalized_axis
+    axis = unnormalized_axis / cs.sqrt(cs.sumsqr(unnormalized_axis) + reg*1e-16)
 
     return theta, axis
