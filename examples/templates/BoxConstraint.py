4ba49ae68948b6a1fb8e0ade412bdaa20ba50726
Ajay Sathya <sajaysathya@gmail.com>
2022-02-08 21:08:38 +0100

    added abstract constraint for imposing box constraints

---
+++ examples/templates/BoxConstraint.py
@@ -0,0 +1,25 @@
+from tasho.ConstraintExpression import ConstraintExpression
+
+def BoxConstraint(expression, lb, ub, name = None):
+
+    """
+    An abstract constraint, that returns a ConstraintExpression object. The returned constraint
+    is a hard box constraint on the expression passed as the parameter. The mid of the constraint
+    is by default "box_constraint_".
+
+    :param expresssion: The expression on which the box constraint is imposed.
+    :type expression: Expression or Variable object.
+
+    :param lb: The lower limits of the box penalty.
+    :type lb: CasADi DM, Python list or Numpy vector.
+
+    :param ub: The upper limits of the box penalty.
+    :type ub: CasADi DM, Python list or Numpy vector.
+
+    :param name: (optional) By default, the uid of the expression.
+    :type name: String
+    """
+
+    if name == None: name = expression.uid
+    con = ConstraintExpression(name, "box_constraint_", expression, "hard", ub = ub, lb = lb)
+
