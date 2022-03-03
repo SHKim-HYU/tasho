99f290c7517867860658264a5ad6564a6d100360
Ajay Sathya <sajaysathya@gmail.com>
2022-03-03 04:48:51 +0100

    removed old template and unused tp_to_binaries files

--- tasho/TimeOptimal.py
+++ tasho/TimeOptimal.py
@@ -10,8 +10,9 @@ from rockit import MultipleShooting, FreeGrid
 class TimeOptimal:
 
     """
-    Class for creating an object that computes time-optimal trajectories for
-    a given set of way-points in the joint space
+    Returns TashoRockit object for the frequently appearing problem of computing time-optimal trajectories for
+    a given set of way-points in the joint space. Meant to be used to compute fast trajectories for joint paths computed using 
+    by software outside Tasho.
     """
 
     def __init__(self, robot, no_waypoints, bounded, control_rate_con = None):
